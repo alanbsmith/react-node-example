bfe6de33ed818458813a15160bf174247d69d03d
Alan B Smith <a.bax.smith@gmail.com>
2018-09-07 10:34:49 -0600

    proof of initial concept

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import 'src/assets/stylesheets/base.scss';
 
+function packageQuery(name, owner) {
+	return gql`
+    query {
+      repository(name: "${name}", owner: "${owner}") {
+        object(expression: "master:package.json") {
+          ... on Blob {
+            text
+          }
+        }
+      }
+    }
+  `;
+}
 
-function App({ name }) {
-  return (
-    <h1>Hello, {name}!</h1>
-  );
-};
+class App extends Component {
+	state = {
+		owner: 'sendgrid',
+		repo: '',
+		dependencies: {}
+	};
 
-App.propTypes = {
-  name: PropTypes.string,
-};
+	handleChange = (e) => {
+		const { id, value } = e.target;
+		return this.setState({ [id]: value });
+	};
 
-export default App;
+	handleSubmit = async (e) => {
+		e.preventDefault();
+		const response = await this.props.client.query({
+			query: packageQuery(this.state.repo, this.state.owner)
+		});
+		const { repository } = response.data;
+		const parsed = JSON.parse(repository.object.text);
+		return this.setState({ dependencies: parsed.dependencies });
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>dep-check</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label htmlFor="owner">owner</label>
+						<input id="owner" onChange={this.handleChange} type="text" value={this.state.owner} />
+					</div>
+					<div>
+						<label htmlFor="repo">repo</label>
+						<input id="repo" onChange={this.handleChange} type="text" value={this.state.repo} />
+						<button type="submit">search</button>
+					</div>
+				</form>
+				<ul>
+					{Object.keys(this.state.dependencies).map((dep, i) => (
+						<li key={i}>
+							{dep}: {this.state.dependencies[dep]}
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default withApollo(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './components/App';
 
-render(<App name='World' />, document.getElementById('root'));
+const client = new ApolloClient({
+	link: new HttpLink({
+		uri: 'https://api.github.com/graphql',
+		headers: {
+			authorization: 'bearer YOUR-TOKEN-HERE'
+		}
+	}),
+	cache: new InMemoryCache()
+});
+
+render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
